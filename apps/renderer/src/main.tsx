7567e2346f218ddab837451fdf247b1dba031cac
harunkurtdev <harunkurt.dev@gmail.com>
2025-04-03 22:22:12 +0300

    proje detayları değiştirildi

--- apps/renderer/src/main.tsx
+++ apps/renderer/src/main.tsx
@@ -1,17 +1,13 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import './main.module.scss';
+import App from './app/App';
 
-import App from './app/app';
-import React from 'react';
+const htmlRoot = document.getElementById('root');
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+if (htmlRoot) {
+  const ReactRoot = ReactDOM.createRoot(htmlRoot);
+
+  ReactRoot.render(<App />);
+} else {
+  console.log('No root HTML element found.');
+}
--- apps/renderer/vite.config.ts
+++ apps/renderer/vite.config.ts
@@ -4,31 +4,17 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../node_modules/.vite/renderer',
+  cacheDir: '../../node_modules/.vite/renderer',
 
   server: {
-    port: 4200,
+    port: Number(process.env.PORT) || 3001,
     host: 'localhost',
   },
 
   preview: {
-    port: 4300,
+    port: Number(process.env.PORT) || 3001,
     host: 'localhost',
   },
 
   plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: '../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
 });
